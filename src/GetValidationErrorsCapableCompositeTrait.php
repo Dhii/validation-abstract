e2a2809a2b56fa3488bd97755e290f9c140120cd
Anton Ukhanev <xedin.unknown@gmail.com>
2018-03-06 09:51:36 +0100

    Converted class to trait
    `_getValidationErrors()` now throws if one of the child valdiators is not a validator.

--- src/GetValidationErrorsCapableCompositeTrait.php
+++ src/GetValidationErrorsCapableCompositeTrait.php
@@ -17,17 +17,23 @@ use Exception as RootException;
  *
  * @since [*next-version*]
  */
-abstract class AbstractCompositeValidator extends AbstractValidator
+trait GetValidationErrorsCapableCompositeTrait
 {
     /**
      * {@inheritdoc}
      *
      * @since [*next-version*]
+     *
+     * @throws OutOfRangeException If one of the child validators is not a validator.
      */
     protected function _getValidationErrors($subject)
     {
         $errors = array();
         foreach ($this->_getChildValidators() as $_idx => $_validator) {
+            if (!($_validator instanceof ValidatorInterface)) {
+                throw $this->_createOutOfRangeException($this->__('Validator %1$s is invalid', [$_idx]), null, null, $_validator);
+            }
+
             try {
                 $_validator->validate($subject);
             } catch (ValidationFailedExceptionInterface $e) {
@@ -57,4 +63,37 @@ abstract class AbstractCompositeValidator extends AbstractValidator
      * @return string[]|Stringable[]|Traversable|stdClass The flat list of validation errors.
      */
     abstract protected function _normalizeErrorList($errorList);
+
+    /**
+     * Creates a new Out Of Range exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable|int|float|bool|null $message  The message, if any.
+     * @param int|float|string|Stringable|null      $code     The numeric error code, if any.
+     * @param RootException|null                    $previous The inner exception, if any.
+     * @param mixed|null                            $argument The value that is out of range, if any.
+     *
+     * @return OutOfRangeException The new exception.
+     */
+    abstract protected function _createOutOfRangeException(
+        $message = null,
+        $code = null,
+        RootException $previous = null,
+        $argument = null
+    );
+
+    /**
+     * Translates a string, and replaces placeholders.
+     *
+     * @since [*next-version*]
+     * @see sprintf()
+     *
+     * @param string $string  The format string to translate.
+     * @param array  $args    Placeholder values to replace in the string.
+     * @param mixed  $context The context for translation.
+     *
+     * @return string The translated string.
+     */
+    abstract protected function __($string, $args = array(), $context = null);
 }
